2955c7f8383094ab59e46541ed59174e4e81fa21
Filip Bulanda <filipbulanda00@gmail.com>
2026-06-26 18:22:00 +0200

    Implement an encoder used to save processed data in new file

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,16 @@ mod types;
 mod decode;
 mod segy_file;
 mod header;
+mod encode;
 
 use pyo3::prelude::*;
-
+use crate::encode::{BinaryHeaderConfig, save_segy};
 use crate::segy_file::SegyFile;
 
 #[pymodule]
 fn _fastsegy(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<SegyFile>()?;
+    m.add_class::<BinaryHeaderConfig>()?;
+    m.add_function(wrap_pyfunction!(save_segy, m)?)?;
     Ok(())
 }
